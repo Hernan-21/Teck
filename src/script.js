a85ac9da32560dd66ac75e2f96ebccd9edc852dd
Hernan-21 <77250341+Hernan-21@users.noreply.github.com>
2021-01-26 01:12:01 -0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(ScrollTrigger)
+
+let container = document.getElementById("wrapper");
+
+gsap.to(container, {
+  x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+  ease: "none",
+  scrollTrigger: {
+    trigger: container,
+    invalidateOnRefresh: true,
+    pin: true,
+    scrub: 1,
+    end: () => "+=" + container.offsetWidth
+  }
+})
